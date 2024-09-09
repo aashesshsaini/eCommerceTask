cb65cbd1ed3cd52aeec4941985601d8d28140521
Er Pratap Mehra <pratapmehra220@outlook.com>
2024-09-09 10:53:59 +0530

    update

--- src/services/user/auth.service.ts
+++ src/services/user/auth.service.ts
@@ -83,7 +83,7 @@ interface loginBody {
   const {zipCode, profileImage, genre, instrument, commitmentlevel, repertoire, document, bio, proficient, improvisationalSkill, motivation, aboutRepertoire, publicExpirence} = body
   console.log(body, "body..........")
   console.log(commitmentlevel, "commitmentlevel.........")
-  const updatedUser = await User.findByIdAndUpdate(userId,{zipCode, profileImage, genre, instrument, commitmentlevel, repertoire, document, bio, proficient, improvisationalSkill, motivation, aboutRepertoire, publicExpirence}, {lean:true, new:true})
+  const updatedUser = await User.findByIdAndUpdate(userId,body, {lean:true, new:true})
   console.log(updatedUser?.commitmentlevel, "updateUser...............")
   if(!updatedUser){
     throw new OperationalError(
